0851c62045e6a86025affbbe3314d5e2410a0fa2
rajesh580 <rajeshrajoli772@gmail.com>
2025-11-10 19:45:06 +0530

    Updates

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders welcome text', () => {
-  render(<App />);
-  const heading = screen.getByText(/Welcome to My Portfolio/i);
-  expect(heading).toBeInTheDocument();
-});
